ac1d327bbd2a2cd3cb69afbfa7db8a756076ec98
SittingDuck1324 <tylerkleiser@gmail.com>
2017-03-10 07:45:58 -0500

    Create module2.js

---
+++ code/module2.js
@@ -0,0 +1,10 @@
+## You can divide
+90/3
+## You can multiply
+3*10
+## You can add
+10+20
+## You can subtract
+100-70
+## You can use the modulus operator Using this will divide the equation and then give you the remainder
+200%170
